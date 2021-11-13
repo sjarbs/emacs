5808a7ca23a55bbe742f80299f7cc548684d2bb8
Sobrino Julian <sobrinojulian@protonmail.com>
2021-11-13 00:30:31 -0300

    initial commit

---
+++ init.el
@@ -0,0 +1,4 @@
+(org-babel-load-file (concat user-emacs-directory "config.org"))
+(custom-set-variables
+ '(package-selected-packages '(doom-themes undo-fu super-save use-package)))
+(custom-set-faces)
